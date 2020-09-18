388cc3dc9129b09c2049c20c0b1d095c23289ca2
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-18 12:56:00 -0700

    revert to working code. state is managed by redux for approx 50% of app. next wil move remaining state to redux store

--- tap-room/src/reducers/bottle-list-reducer.js
+++ tap-room/src/reducers/bottle-list-reducer.js
@@ -1,17 +1,14 @@
-import * as c from './../actions/ActionTypes';
+import * as c from '../actions/ActionTypes';
 
 export default (state = {}, action) => {
-  const { name, type, price, origin, tastingNotes, count, id } = action;
+  const { names, location, issue, id } = action;
   switch (action.type) {
   case c.ADD_BOTTLE:
     return Object.assign({}, state, {
       [id]: {
-        name: name,
-        type: type,
-        price: price,
-        origin: origin,
-        tastingNotes: tastingNotes,
-        count: count,
+        names: names,
+        location: location,
+        issue: issue,
         id: id
       }
     });
