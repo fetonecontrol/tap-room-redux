369b58624043c736640bfd2a09be95617557c313
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-18 19:26:20 -0700

    selected bottle is registered correctly at the details page, but is reset to null shortly after.

--- tap-room/src/reducers/selected-bottle-reducer.js
+++ tap-room/src/reducers/selected-bottle-reducer.js
@@ -4,8 +4,9 @@ import * as c from './../actions/ActionTypes';
 export default (state = null, action) => {
   switch (action.type) {
   case c.SELECTED_BOTTLE:
+    console.log(state);
     return Object.assign({}, state, action);
   default:
-    return null;
+    return state;
   }
 };
